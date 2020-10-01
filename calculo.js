b1413b407ea494adf6994fbe9c186352cf1c87df
Nichollas <nichollasdsfreitas@gmail.com>
2020-10-01 12:22:49 +0000

    Initial commit

---
+++ calculo.js
@@ -0,0 +1,39 @@
+const calcular = document.getElementById('calcular');
+
+
+function imc () {
+  const nome = document.getElementById('nome').value;
+  const altura = document.getElementById('altura').value;
+  const peso = document.getElementById('peso').value;
+  const resultado = document.getElementById('resultado');
+  
+  
+  if (nome !== '' && altura !== '' && peso !== '') {
+    const valorIMC = (peso/(altura*altura)).toFixed(1);
+
+    let classificacao = ''
+
+    if (valorIMC < 18.5){
+      classificacao = 'abaixo peso.';
+    }else if (valorIMC < 25) {
+      classificacao = 'peso ideal.';
+    }else if (valorIMC < 30) {
+      classificacao = 'pré-obeso.';
+    }else if (valorIMC < 35) {
+      classificacao = 'obeso grau I.';
+    }else if (valorIMC < 40) {
+      classificacao = 'obeso grau II.';
+    }else if (valorIMC >= 40) {
+      classificacao = 'obeso grau III, cuidado!';
+    }
+
+    resultado.textContent = `${nome} seu IMC é ${valorIMC} e você está ${classificacao}`;
+
+    }else {
+    resultado.textContent = 'Preencha todos os campos!';
+  }
+
+  }
+  
+
+calcular.addEventListener("click", imc);
